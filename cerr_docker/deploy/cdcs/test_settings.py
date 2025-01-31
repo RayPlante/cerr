b55597e68a7a1a6a776ee2ad175ec320147e31fc
snb24@nist.gov <samia.benjida@nist.gov>
2025-01-31 11:01:59 -0500

    update cdcs-docker deploy

---
+++ cerr_docker/deploy/cdcs/test_settings.py
@@ -0,0 +1,6 @@
+""" Test Settings
+"""
+
+from .settings import *  # import default settings from image
+
+# TODO: Add or override settings below
---
+++ cerr_docker/deploy/docker_migrate.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+source .env
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --dry-run --no-input
+read -p "Press enter to apply changes:"
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --no-input
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate --plan
+read -p "Press enter to apply changes:"
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate
