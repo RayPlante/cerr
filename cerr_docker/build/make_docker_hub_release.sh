1bc071b13a2c9bf44eeca3b4886efa5822ec781c
snb24@nist.gov <samia.benjida@nist.gov>
2025-02-03 19:50:05 -0500

    upgrade cdcs3.13 -- build folder

--- cerr_docker/build/make_docker_hub_release.sh
+++ cerr_docker/build/make_docker_hub_release.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${DIR}/.env"
 
 
-echo "Tagging ${IMAGE_NAME}:${PROJECT_VERSION} as nistodi/cdcs_ce:${PROJECT_VERSION}"
-docker tag ${IMAGE_NAME}:${PROJECT_VERSION} nistodi/cdcs_ce:${PROJECT_VERSION}
+echo "Tagging ${IMAGE_NAME}:${IMAGE_VERSION} as nistodi/cdcs_ce:${IMAGE_VERSION}"
+docker tag ${IMAGE_NAME}:${IMAGE_VERSION} nistodi/cdcs_ce:${IMAGE_VERSION}
 
-echo "Pushing nistodi/cdcs_ce:${PROJECT_VERSION} to Docker Hub"
-docker push nistodi/cdcs_ce:${PROJECT_VERSION}
+echo "Pushing nistodi/cdcs_ce:${IMAGE_VERSION} to Docker Hub"
+docker push nistodi/cdcs_ce:${IMAGE_VERSION}
